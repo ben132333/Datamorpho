54e8036cff1eaa4cfae17225c1029114c368f860
Ben Dumoulin <31484050+ben132333@users.noreply.github.com>
2022-08-21 09:08:57 +0000

    Decryption funcvtional

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
+// https://ipfs.filebase.io/ipfs/QmUd3Nu7xyJigViChGpDdqg1NaA6xnKYjTfgkcF6Yy3LQq
 
+document.getElementById('btn').onclick = function() {
 
-let url = 'https://ipfs.filebase.io/ipfs/QmUd3Nu7xyJigViChGpDdqg1NaA6xnKYjTfgkcF6Yy3LQq';
+  var picture = document.getElementById("picture");
+  
+  var metadata_url = document.getElementById('imagename').value,
+    key_value = document.getElementById('keyvalue').value,
+    img = document.createElement('img');
 
-fetch(url)
-.then(res => res.json())
-.then((out) => {
-  var image_url = out.image;
-  document.write("image_url: ", image_url);
-})
-.catch(err => { throw err });
+  fetch(metadata_url)
+  .then(res => res.json())
+  .then((out) => {
+    var image_url = out.image;
+    var secret_image_1 = out.secret_image_1;
+
+    img.src = image_url;
+    img.width = "500";
+    img.height = "600";
+    // document.body.appendChild(img);
+    picture.src = image_url;
+ 
+    if (key_value !== "" && secret_image_1 !== "") {
+      var data = new XMLHttpRequest();
+      data.open('GET', secret_image_1, true);
+      
+      data.onreadystatechange = function(){
+          if(this.readyState == 4 && this.status==200){
+              var dec = CryptoJS.AES.decrypt(data.responseText, key_value);
+              var plain = CryptoJS.enc.Base64.stringify( dec );
+          picture.src = "data:image/jpeg;base64,"+plain;
+          }
+      };
+      data.send(null);
+      
+    };
+  
+  })
+  .catch(err => { throw err });
+  
+}
